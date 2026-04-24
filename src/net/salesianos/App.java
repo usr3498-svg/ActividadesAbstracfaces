0d162b9b3d3bac86b361808438dae806a2e07d3e
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-24 23:17:14 +0100

    Creación código App.java
    Creación de la clase ejecutable, donde se introducen los valores para que las interfaces publicas usen sus parámetros para medir sus radios y perímetros.

--- src/net/salesianos/App.java
+++ src/net/salesianos/App.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
+import CalculoGeometrico.Circulo;
+import CalculoGeometrico.Rectangulo;
+
 public class App {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la base del rectangulo");
+        double base = sc.nextDouble();
+
+        System.out.println("Introduce la altura del rectangulo");
+        double altura = sc.nextDouble();
+
+        Rectangulo rectangulo = new Rectangulo(base, altura);
+
+        System.out.println("El perimetro del rectangulo es igual a: " + rectangulo.Perimetro());
+        System.out.println("El area del rectangulo es igual a: " + rectangulo.Area());
+
+        System.out.println("Introduce el radio del círculo:");
+        double radio = sc.nextDouble();
+
+        Circulo circulo = new Circulo(radio);
+
+        System.out.println("El perimetro del circulo es igual a: " + circulo.Perimetro());
+        System.out.println("El area del circulo es igual a: " + circulo.Area());
+
+
+    }
+
 }
