a48b6e0a36cc97fd2c59c0a98c9cca6c0d571c91
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-25 01:34:24 +0100

    Update App.java
    Update del App.java en el cual incluimos aparte de las figuras geométricas la clases abstractas y todo el tema de Vehículos.

--- src/net/salesianos/App.java
+++ src/net/salesianos/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import CalculoGeometrico.Circulo;
 import CalculoGeometrico.Rectangulo;
+import Vehiculos.CocheCombustion;
 
 public class App {
 
@@ -27,6 +28,39 @@ public class App {
 
         System.out.println("El perimetro del circulo es igual a: " + circulo.Perimetro());
         System.out.println("El area del circulo es igual a: " + circulo.Area());
+        System.out.println("_________________________________________________________________");
+
+        CocheCombustion coche = new CocheCombustion("Blanco", 4, 4, "Ford Fiesta", "1986AZB", 20 ); 
+
+        System.out.println("Intento mover el coche sin arrancar:");
+        coche.mover();
+        System.out.println("------------");
+
+        
+        
+        System.out.println("Arranco el coche:");
+        coche.arrancar(); 
+                System.out.println("------------");
+        
+        System.out.println("Muevo el coche varias veces:");
+        coche.mover(); 
+        coche.mover(); 
+        coche.mover();
+        coche.mover();
+        coche.mover();
+        System.out.println("------------");
+       
+        System.out.println("Combustible restante: " + coche.getLitrosCombustible());
+        System.out.println("------------");
+      
+        System.out.println("Me roban la gasolina y me dejan solo 2 de combustible:");
+        coche.setLitrosCombustible(2);
+        System.out.println("------------");
+
+        System.out.println("Muevo el coche 3 veces más:");
+        coche.mover(); 
+        coche.mover();
+        coche.mover(); 
 
 
     }
